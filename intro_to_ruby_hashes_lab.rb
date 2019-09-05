6b7c45f08a5bb4291e6c4b46f704629b3a43def2
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-05 23:30:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,10 @@ def monopoly_with_third_tier
 	monopoly = {
 	  :railroads => {
 	    :pieces => 4,
-	    :names => {}
+	    :names => {},
+	    :rent_in_dollars => {
+	      :
+	    }
 	  }
 	  }
 end
